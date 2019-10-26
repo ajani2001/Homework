20027f8de11cb56d50e657a963acabda5e3b2827
Grigory Ivanov <ivanovgrigoriinikolaevich00@gmail.com>
2019-10-26 08:47:48 +0700

    commit

--- Lab0/Lab0/Main.cpp
+++ Lab0/Lab0/Main.cpp
@@ -13,79 +13,85 @@ template <typename T> void copyArray(const T* source, T* dest, size_t arrayLengt
 
 class RNA {
 private:
-	size_t capacity;
-	size_t length;
+	size_t storageSize;
 	unsigned int* storage;
+
 	void resize(size_t newSize) {
 		unsigned int* newStorage = new unsigned int[newSize];
-		copyArray<unsigned int>(storage, newStorage, capacity < newSize ? capacity : newSize);
+		copyArray<unsigned int>(storage, newStorage, storageSize < newSize ? storageSize : newSize);
 		delete[] storage;
-		capacity = newSize;
+		storageSize = newSize;
 		storage = newStorage;
 	}
 
-	class StorageAccessorBase {
-	protected:
-		size_t nucleotideIndex;
-		size_t bitPackIndex;
-		size_t bitPairIndex;
-	public:
-		void setNewIndex(size_t newIndex) {
-			nucleotideIndex = newIndex;
-			bitPackIndex = nucleotideIndex / (4 * sizeof(unsigned int));
-			bitPairIndex = nucleotideIndex % (4 * sizeof(unsigned int));
+	void fitSize() {/*
+		if (length / 4 > sizeof(unsigned int)*storageSize * 99 / 100 || length / 4 < sizeof(unsigned int)*storageSize * 90 / 100) {
+			resize(length / 4 / 95 * 100 / sizeof(unsigned int));
+		}*/
+		size_t bytesRequired = (length + 3) / 4;
+		size_t bytesAllocated = storageSize * sizeof(unsigned int);
+		if (bytesRequired + 100 >= bytesAllocated) {
+			resize(storageSize + 1000);
+			return;
 		}
-		virtual operator Nucleotide () const = 0; // not good
-	};
-	class StorageAccessor: public StorageAccessorBase {
+		if (bytesRequired + 1900 <= bytesAllocated) {
+			resize(storageSize - 1000);
+			return;
+		}
+	}
+
+	Nucleotide getNucleotide(size_t nucleotideIndex) const {
+		size_t bitPackIndex = nucleotideIndex / (4 * sizeof(unsigned int));
+		size_t bitPairIndex = nucleotideIndex % (4 * sizeof(unsigned int));
+		unsigned int bitPack = storage[bitPackIndex];
+		return Nucleotide((bitPack >> bitPairIndex)&mask);
+	}
+
+	void setNucleotide(size_t nucleotideIndex, Nucleotide newValue) {
+		if (nucleotideIndex >= length) {
+			throw 1;
+		}
+		size_t bitPackIndex = nucleotideIndex / (4 * sizeof(unsigned int));
+		size_t bitPairIndex = nucleotideIndex % (4 * sizeof(unsigned int));
+		unsigned int bitPack = storage[bitPackIndex];
+		storage[bitPackIndex] = (bitPack&(~(mask << bitPairIndex))) | ((unsigned int(newValue)) << bitPairIndex);
+	}
+
+	class StorageAccessor {
 	private:
 		RNA* proprietor;
+		size_t nucleotideIndex;
 	public:
-		StorageAccessor(RNA* proprietor, size_t nucleotideIndex) : proprietor(proprietor) {
-			setNewIndex(nucleotideIndex);
-		}
+		StorageAccessor(RNA* proprietor, size_t nucleotideIndex) : proprietor(proprietor), nucleotideIndex(nucleotideIndex) {}
 		operator Nucleotide () const {
-			unsigned int bitPack = proprietor->storage[bitPackIndex];
-			return Nucleotide((bitPack >> bitPairIndex)&mask);
+			return proprietor->getNucleotide(nucleotideIndex);
 		}
 		Nucleotide operator= (Nucleotide newValue) const {
-			unsigned int bitPack = proprietor->storage[bitPackIndex];
-			proprietor->storage[bitPackIndex] = (bitPack&(~(mask << bitPairIndex))) | ((unsigned int(newValue)) << bitPairIndex);
+			proprietor->setNucleotide(nucleotideIndex, newValue);
 			return newValue;
-		}
-	};
-	class StorageAccessorConst : public StorageAccessorBase {
-	private:
-		const RNA* proprietor;
-	public:
-		StorageAccessorConst(const RNA* proprietor, size_t nucleotideIndex) : proprietor(proprietor) {
-			setNewIndex(nucleotideIndex);
-		}
-		operator Nucleotide () const {
-			unsigned int bitPack = proprietor->storage[bitPackIndex];
-			return Nucleotide((bitPack >> bitPairIndex)&mask);
 		}
 	};
 
 public:
-	RNA() : capacity(1000), length(0), storage(new unsigned int[1000]) {}
-	RNA(const RNA& rna): capacity(rna.capacity), length(rna.length) {
-		storage = new unsigned int[rna.capacity];
-		copyArray<unsigned int>(rna.storage, storage, capacity);
+	size_t length;
+	RNA() : storageSize(0), length(0), storage(nullptr) {}
+	RNA(const RNA& rna): storageSize(rna.storageSize), length(rna.length) {
+		storage = new unsigned int[rna.storageSize];
+		copyArray<unsigned int>(rna.storage, storage, storageSize);
 	}
 	RNA(Nucleotide filler, int fillLength) : length(fillLength) {
-		capacity = fillLength / sizeof(int) + (fillLength % sizeof(int) == 0 ? 0 : 1);
-		storage = new unsigned int[capacity];
-		StorageAccessor iterator(this, 0);
-		for (int i = 0; i < fillLength; iterator.setNewIndex(++i)) {
-			iterator = filler;
+		storageSize = 0;
+		storage = nullptr;
+		fitSize();
+		for (int i = 0; i < fillLength; ++i) {
+			setNucleotide(i, filler);
 		}
 	}
 	StorageAccessor operator[] (size_t nucleotideIndex) {
 		return StorageAccessor(this, nucleotideIndex);
 	}
-	StorageAccessorConst operator[] (size_t nucleotideIndex) const {
-		return StorageAccessorConst(this, nucleotideIndex);
+	Nucleotide operator[] (size_t nucleotideIndex) const {
+		return getNucleotide(nucleotideIndex);
 	}
 	bool operator== (const RNA& rvalue) const {
 		if (length != rvalue.length) {
@@ -101,25 +107,48 @@ public:
 	bool operator!= (const RNA& rvalue) const {
 		return !operator==(rvalue);
 	}
-	RNA& operator~() const { // dont understand
+	RNA operator~() const {
 		RNA result(*this);
-		for (int i = 0; i < capacity; ++i) {
+		for (int i = 0; i < storageSize; ++i) {
 			result.storage[i] = ~(result.storage[i]);
 		}
-		return result; // is this legal?
+		return result;
 	}
 	RNA& operator=(const RNA& rvalue) {
 		delete[] storage;
-		capacity = rvalue.capacity;
+		storageSize = rvalue.storageSize;
 		length = rvalue.length;
-		storage = new unsigned int[capacity];
-		copyArray<unsigned int>(rvalue.storage, storage, capacity);
+		storage = new unsigned int[storageSize];
+		copyArray<unsigned int>(rvalue.storage, storage, storageSize);
 		return *this;
 	}
-	RNA& operator+(const RNA& rvalue);
-	RNA& operator+=(const RNA& rvalue);
-	void trim(size_t length);
-
+	RNA operator+(const RNA& rvalue) {
+		RNA result(*this);
+		result += rvalue;
+		return result;
+	}
+	RNA& operator+=(const RNA& rvalue) {
+		length += rvalue.length;
+		for (int i = 0; i < rvalue.length; ++i) {
+			(*this) += rvalue[i];
+		}
+		return *this;
+	}
+	RNA operator+(Nucleotide rvalue) {
+		RNA result(*this);
+		result += rvalue;
+		return result;
+	}
+	RNA& operator+=(Nucleotide rvalue) {
+		++length;
+		fitSize();
+		setNucleotide(length - 1, rvalue);
+		return *this;
+	}
+	void trim(size_t newLength) {
+		length = newLength;
+		fitSize();
+	}
 	~RNA() {
 		delete[] storage;
 		storage = nullptr;
@@ -136,7 +165,56 @@ public:
 		}
 	}
 };
+ostream& operator<<(ostream& os, const DNA& dna) {
+	for (int i = 0; i < dna.rna1.length; ++i) {
+		Nucleotide currentNucl = dna.rna1[i];
+		char currentChar;
+		switch (currentNucl) {
+		case A:
+			currentChar = 'A';
+			break;
+		case G:
+			currentChar = 'G';
+			break;
+		case C:
+			currentChar = 'C';
+			break;
+		case T:
+			currentChar = 'U';
+			break;
+		}
+		os << currentChar;
+	}
+	return os;
+}
+ostream& operator<<(ostream& os, const RNA& rna) {
+	for (int i = 0; i < rna.length; ++i) {
+		Nucleotide currentNucl = rna[i];
+		char currentChar;
+		switch (currentNucl) {
+		case A:
+			currentChar = 'A';
+			break;
+		case G:
+			currentChar = 'G';
+			break;
+		case C:
+			currentChar = 'C';
+			break;
+		case T:
+			currentChar = 'T';
+			break;
+		}
+		os << currentChar;
+	}
+	return os;
+}
 
 int main() {
+	RNA rna1;
+	RNA rna2(A, 300);
+	RNA rna3(~rna2);
+	DNA dna1(rna2, rna3);
+	cout << "rna2: " << rna2 << endl << "rna3: " << rna3 << endl << "dna1: " << dna1 << endl;
 	return 0;
 }
